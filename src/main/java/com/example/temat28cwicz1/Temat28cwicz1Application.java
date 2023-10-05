d75a53f2fedd1c3cf410c9d3e5a4583837656b9e
Pawel Maliszewski <pablo999455@gmail.com>
2023-10-05 14:24:46 +0200

    master

---
+++ src/main/java/com/example/temat28cwicz1/Temat28cwicz1Application.java
@@ -0,0 +1,13 @@
+package com.example.temat28cwicz1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Temat28cwicz1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Temat28cwicz1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/temat28cwicz1/Temat28cwicz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temat28cwicz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Temat28cwicz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
